6da469d63ab1fd514d8c8732d88f9ca538fcfe50
Robert Mejia <robertmejiasaravia@outlook.com>
2025-08-11 16:27:41 -0500

    actualizacion de func

--- funciones.py
+++ funciones.py
@@ -1,4 +1,8 @@
-def alex():
-    print('Hola Alex')
-
-alex()
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+def suma(a, b):
+    return a + b
